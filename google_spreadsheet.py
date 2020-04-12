20f457b7d2a4e911af15828ecc66c91d9cd49dca
Gerard Keating <gkeating@groupon.com>
2020-04-12 14:54:17 +0100

    Now reads from google sheets and prints csv

--- google_spreadsheet.py
+++ google_spreadsheet.py
@@ -9,8 +9,46 @@ from google.auth.transport.requests import Request
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 # The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
-SAMPLE_RANGE_NAME = 'Class Data!A2:E'
+SAMPLE_SPREADSHEET_ID = '1n0VpO6nk2Zr3yNrE00PrmXeKInooJPl1Y1h5j2riwGE'
+SAMPLE_RANGE_NAME = 'Sheet1'
+
+def get_movie_list():
+    creds = get_creds()
+    service = build('sheets', 'v4', credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                range='Sheet1!B1:B').execute()
+    values = result.get('values', [])
+
+    if not values:
+        raise Exception("No data found in spreadsheet")
+    # removed header
+    return [row[0] for row in values][1:]
+
+
+def get_creds():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    return creds
+
 
 def main():
     """Shows basic usage of the Sheets API.
@@ -46,10 +84,8 @@ def main():
     if not values:
         print('No data found.')
     else:
-        print('Name, Major:')
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s, %s' % (row[0], row[4]))
+        from pprint import pprint
+        pprint(values)
 
 if __name__ == '__main__':
     main()
--- movies.py
+++ movies.py
@@ -1,32 +1,62 @@
 import string
 import urllib.parse
 import requests
+import google_spreadsheet
+import logging 
 
 import secrets
 
 def get_movie_list():
-    with open("movie_list.txt") as fp:
-        raw_string = fp.read()
-    res = raw_string.split("\n")
-    res = [i for i in res if i]
-    return res
+    return google_spreadsheet.get_movie_list()
+
+def is_on_netflix(movie_name):
+    movie_name = movie_name.strip()
+    if ord(movie_name[-1]) ==10060:
+        return False
+    elif ord(movie_name[-1]) ==9989:
+        return True
+    return None
 
 def main():
     lines = get_movie_list()
+    i = 1
     for movie in lines:
+        print(i, end=',')
+        i+=1
+        print(movie, end=',')
+        on_netflix = is_on_netflix(movie)
+        if on_netflix is None:
+            print(" ", end=',')
+        elif on_netflix:
+            print("yes", end=',')
+        else:
+            print("no", end=',')
+
         clean_name = ""
         for c in movie:
             if c in string.printable:
                 clean_name += c
         clean_name = clean_name.strip()
-        # print(clean_name)
+        logging.debug(clean_name)
         clean_name = urllib.parse.quote_plus(clean_name)
-        # movies.append(clean_name)
-        r= requests.get("http://www.omdbapi.com/?apikey=%s&t=%s&type=movie"%(
-            secrets.api_key, 
-            clean_name)).json()
-        print(r['Year'])
-        # if True:
+        # # movies.append(clean_name)
+        r = requests.get("http://www.omdbapi.com/?apikey=%s&t=%s&type=movie"%(
+                        secrets.api_key, 
+                        clean_name)).json()
+        try:
+            id_ = r['imdbID']
+        except KeyError:
+                 raise Exception("Title %s not found" % clean_name)
+        print("https://www.imdb.com/title/" + r['imdbID'], ",")
+        for d in r['Ratings']:
+            if d['Source']=='Internet Movie Database':
+                print(d['Value'].strip("/10"), end=",")
+                break
+        else:
+            raise Exception("IMDB rating not found in: %s "% r)
+
+        print(r['Year'], end=",")
+        # # if True:
         #     try:
         #         id_ = r['imdbID']
         #     except KeyError:
@@ -34,13 +64,10 @@ def main():
         #     print("https://www.imdb.com/title/" + r['imdbID'])
         # else:
         #     found = False
-        #     for d in r['Ratings']:
-        #         if d['Source']=='Internet Movie Database':
-        #             print(d['Value'].strip("/10"))
-        #             found = True
+        #     
         #     if not found:
         #         raise Exception(r)
-
+        print("")
 if __name__=="__main__":
     main()
 
